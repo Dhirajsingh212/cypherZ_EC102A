222c15d73bf8daa35bc97354f352070bd0c0fe95
Dhirajsingh212 <dhirajsingh0229@gmail.com>
2024-10-04 17:27:04 +0530

    updated

--- Shop-Topia/src/Components/Navbar/Navbar.jsx
+++ Shop-Topia/src/Components/Navbar/Navbar.jsx
@@ -56,7 +56,7 @@ export const Navbar = () => {
                   <div className="w-10 rounded-full">
                     <img
                       alt="Tailwind CSS Navbar component"
-                      src="https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp"
+                      src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp"
                     />
                   </div>
                 </div>
